4e1975b7d62140213291ea79eca37676defbf18c
zicozydailva <zicozydasilva70@gmail.com>
2023-04-09 06:31:25 +0100

    pushDLL

--- DATA STRUCTURE/02.DOUBLY_LINKED_LIST/index.js
+++ DATA STRUCTURE/02.DOUBLY_LINKED_LIST/index.js
@@ -13,6 +13,24 @@ class DoublyLinkedList {
     this.length = 0;
   }
 
-  push() {}
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length == 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
   pop() {}
 }
+
+const list = new DoublyLinkedList();
+list.push(10);
+list.push(11);
+
+console.log(list);
